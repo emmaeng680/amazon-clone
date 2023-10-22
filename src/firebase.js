8b934ba65e5130f8e14b25b2f8faba28d9a99ed4
Emmanuel Oppong <oppongemma73@gmail.com>
2023-10-23 02:49:10 +0400

    update

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAoTC2lq8z4OPlfjofChJ8w15oDYlcsn9Y",
+  authDomain: "challenge-56240.firebaseapp.com",
+  projectId: "challenge-56240",
+  storageBucket: "challenge-56240.appspot.com",
+  messagingSenderId: "154789337033",
+  appId: "1:154789337033:web:8964a1fc34ad7b630d3a18"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
